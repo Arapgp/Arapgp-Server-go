5e5c989545812881b1b05ff8f251f83dedb6b4c0
skyleaworlder <870033938@qq.com>
2021-05-07 20:13:51 +0800

    M: add urlBase in test/setup_teardown.go

--- test/setup_teardown.go
+++ test/setup_teardown.go
@@ -1,18 +1,30 @@
 package test
 
 import (
+	"strconv"
 	"testing"
 
 	"github.com/Arapgp/Arapgp-Server-go/config"
+	"github.com/Arapgp/Arapgp-Server-go/route"
+	"github.com/gin-gonic/gin"
 	log "github.com/sirupsen/logrus"
 )
 
+var urlBase string
+
 func setupConfig(t *testing.T) {
 	config.Setup("../arapgp.server.json")
-	log.Info("Setup, Test Begin")
+	urlBase = "http://" + config.Svccfg.Host + ":" + strconv.Itoa(config.Svccfg.Port)
+
+	log.Info("Setup, Test Begin, url base is:", urlBase)
 }
 
 func teardownConfig(t *testing.T) {
 	config.Teardown()
+	urlBase = ""
 	log.Info("Teardown, Test End")
+}
+
+func setupRouter(t *testing.T) (r *gin.Engine) {
+	return route.InitRouter()
 }

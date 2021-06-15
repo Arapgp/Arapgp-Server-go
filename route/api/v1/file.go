a148226dd56d448fce9dd2658d151fb6104dc769
skyleaworlder <870033938@qq.com>
2021-06-15 15:08:12 +0800

    M: fix error about add Key 'UserName' to API-JSON, :username has been ready in url-param

--- route/api/v1/file.go
+++ route/api/v1/file.go
@@ -12,6 +12,7 @@ import (
 
 // PostFileByUserName is to "POST PGPFile" by username & file name
 func PostFileByUserName(c *gin.Context) {
+	username := c.Param("username")
 	var json JSONPostPutFile
 	if err := c.ShouldBindJSON(&json); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"status": "Bad" + c.Request.Method + "request!"})
@@ -19,10 +20,16 @@ func PostFileByUserName(c *gin.Context) {
 	}
 
 	// get users PubKey(insert pgpfile needed)
+	// check PubKey: (1) is null? (2) is matched?
 	users := make([]model.User, 1)
-	err := model.GetUsers(users, bson.M{"profile.name": json.UserName})
+	err := model.GetUsers(users, bson.M{"profile.name": username})
 	if err != nil || users[0].Profile.Name == "" {
-		c.JSON(http.StatusOK, gin.H{"status": "User does not exist"})
+		c.JSON(http.StatusOK, gin.H{"status": "User does not exist!"})
+		return
+	}
+	if users[0].PubKey != json.PubKey || users[0].PubKey == "" {
+		c.JSON(http.StatusOK, gin.H{"status": "Public Key do not qualified!"})
+		return
 	}
 
 	// check whether file already exists
@@ -30,9 +37,9 @@ func PostFileByUserName(c *gin.Context) {
 	err = model.GetPGPFiles(checkFiles, bson.D{{Key: "user", Value: "?"}})
 	files := []model.PGPFile{
 		{
-			Name: json.Name, Author: json.UserName, Size: len(json.Content),
+			Name: json.Name, Author: username, Size: len(json.Content),
 			CreateTime: time.Now(), LastModifyTime: time.Now(),
-			Path: "/" + json.UserName + "/" + json.Name, PubKey: users[0].PubKey,
+			Path: "/" + username + "/" + json.Name, PubKey: json.PubKey,
 		},
 	}
 	err = model.InsertPGPFiles(files)
@@ -47,12 +54,15 @@ func PostFileByUserName(c *gin.Context) {
 		bson.M{"$push": bson.M{
 			"files": bson.M{"$each": files},
 		}},
-		bson.D{{Key: "profile.name", Value: json.UserName}},
+		bson.D{{Key: "profile.name", Value: username}},
 	)
 	if err != nil {
 		c.JSON(http.StatusOK, gin.H{"status": "Unexpected error!"})
 		return
 	}
+
+	c.JSON(http.StatusOK, gin.H{"status": "OK"})
+	return
 }
 
 // PutFileByUserName is to "PUT PGPFile" by username & file name
@@ -93,7 +103,7 @@ func GetFilesInfoByUserName(c *gin.Context) {
 
 // JSONPostPutFile is a json for Post / Put File
 type JSONPostPutFile struct {
-	Name     string `json:"name" binding:"required"`
-	UserName string `json:"username" binding:"required"`
-	Content  string `json:"content" binding:"required"`
+	Name    string `json:"name" binding:"required"`
+	Content string `json:"content" binding:"required"`
+	PubKey  string `json:"pubKey" binding:"required"`
 }

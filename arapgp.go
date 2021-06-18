268f871f368f69db9f2a6969690d8d3b97e048fa
skyleaworlder <870033938@qq.com>
2021-06-18 20:40:50 +0800

    M: use default, have a try?

--- arapgp.go
+++ arapgp.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"strconv"
-	"time"
 
 	"github.com/Arapgp/Arapgp-Server-go/config"
 	"github.com/Arapgp/Arapgp-Server-go/route"
@@ -14,13 +13,7 @@ func main() {
 	router := route.InitRouter()
 
 	// CORS
-	router.Use(cors.New(cors.Config{
-		AllowAllOrigins:  true,
-		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
-		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type"},
-		AllowCredentials: false,
-		MaxAge:           12 * time.Hour,
-	}))
+	router.Use(cors.Default())
 
 	router.Run(":" + strconv.Itoa(config.Svccfg.Port))
 }

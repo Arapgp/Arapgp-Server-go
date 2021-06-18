07580e42d468f7f03953102c415f554e8971f993
skyleaworlder <870033938@qq.com>
2021-06-18 12:30:17 +0800

    M: cors fixed

--- arapgp.go
+++ arapgp.go
@@ -2,14 +2,25 @@ package main
 
 import (
 	"strconv"
+	"time"
 
 	"github.com/Arapgp/Arapgp-Server-go/config"
 	"github.com/Arapgp/Arapgp-Server-go/route"
+	"github.com/gin-contrib/cors"
 )
 
 func main() {
 	config.Setup("./arapgp.server.json")
 	router := route.InitRouter()
+
+	// CORS
+	router.Use(cors.New(cors.Config{
+		AllowAllOrigins:  true,
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type"},
+		AllowCredentials: false,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	router.Run(":" + strconv.Itoa(config.Svccfg.Port))
 }
